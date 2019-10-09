544bd1844f8a6689053b9a6f2dfc4772142fd4e1
Walter Satriano <walter.satri@gmail.com>
2019-10-09 14:44:11 +0200

    creato model Post

---
+++ app/Post.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+  public function category() {
+    return $this -> belongsTo(Category::class);
+  }
+  protected $fillable = [
+    'category_id',
+    'title',
+    'text'
+  ];
+}
