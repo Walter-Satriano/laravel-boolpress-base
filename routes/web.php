fba6360b68c9a5101076927860eae1dc0d72bcc5
Walter Satriano <walter.satri@gmail.com>
2019-10-09 15:28:18 +0200

    definite rotte

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'BlogController@index') -> name('home-page');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/category/{id}', 'BlogController@showCategory') -> name('categories');
+Route::get('/post/{id}', 'BlogController@showPost') -> name('posts');
+
+Route::get('/admin/post/create', 'BlogController@create') -> name('create.post');
+Route::post('/admin/post/store', 'BlogController@store') -> name('store.post');
+
+Route::get('/admin/post/edit/{id}', 'BlogController@edit') -> name('edit.post');
+Route::post('/admin/post/update/{id}', 'BlogController@update') -> name('update.post');
+
+Route::get('/admin/post/destroy/{id}', 'BlogController@destroy') -> name('destroy.post');
